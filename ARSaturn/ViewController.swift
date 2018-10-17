a6a676ec0e29b639abcdadea8f355791f589a3ea
suyeon <synature14@gmail.com>
2018-10-17 14:01:51 +0900

    sun, mercury추가

--- ARSaturn/ViewController.swift
+++ ARSaturn/ViewController.swift
@@ -16,15 +16,27 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     @IBOutlet weak var checkLabel: UILabel!
     
     let fadeDuration: TimeInterval = 0.3
-    let rotateDuration: TimeInterval = 3
+    let rotateDuration: TimeInterval = 5
     let waitDuration: TimeInterval = 0.5
+    
+    enum Planet: String {
+        case sun = "Sun"
+        case mercury = "mercury"
+        case venus = "venus"
+        case earth = "earth"
+        case moon = "moon"
+        case marse = "marse"
+        case saturn = "saturn"
+    }
+    
+    var planetType: Planet!
     
     lazy var fadeAndSpinAction: SCNAction = {
         return .sequence([
             .fadeIn(duration: fadeDuration),
-            .rotateBy(x: 0, y: 0, z: CGFloat.pi * 360 / 180, duration: rotateDuration),
-            .wait(duration: waitDuration),
-            .fadeOut(duration: fadeDuration)
+            .rotateBy(x: 0, y:  CGFloat.pi * 360 / 180, z: 0, duration: rotateDuration),
+            .wait(duration: waitDuration)
+//            .fadeOut(duration: fadeDuration)
             ])
     }()
     
@@ -36,22 +48,32 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             ])
     }()
     
+    lazy var spinAction: SCNAction = {
+        let spinAction = SCNAction.rotateBy(x: 0, y:  CGFloat.pi * 360 / 180, z: 0, duration: rotateDuration)
+        let repeatAction = SCNAction.repeatForever(spinAction)
+        return repeatAction
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Show statistics such as fps and timing information
         sceneView.showsStatistics = true
-        
+
         // Create a new scene
-        let saturnScene = SCNScene(named: "saturn.scn")!
+        let solarScene = SCNScene(named: "solarSystem.scn")!
+        planetType = .sun
         
         // Set the scene to the view
-        sceneView.scene = saturnScene
+        sceneView.scene = solarScene
+        sceneView.scene.rootNode.childNodes.map { $0.runAction(spinAction) }
         configureLighting()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        resetTrackingConfiguration()    // Create a session configuration
+        let config = ARWorldTrackingConfiguration()
+        sceneView.session.run(config, options: ARSession.RunOptions.resetTracking)
+//        resetTrackingConfiguration()    // Create a session configuration
     }
     
     override func viewWillDisappear(_ animated: Bool) {
@@ -70,24 +92,30 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         resetTrackingConfiguration()
     }
     
-    
+   /*
     // When detected image!
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         guard let imageAnchor = anchor as? ARImageAnchor else { return }
         let referenceImage = imageAnchor.referenceImage
-        let imageName = referenceImage.name ?? "no name"
+        
+        
+        let imageName = referenceImage.name ?? "None"
+       
+        switch imageName {
+        case :
+        default: break
+        }
         
         guard let overlayNode = self.node(with: imageName) else {
             return
         }
+        let planeNode = self.planeNode(withReferenceImage: imageAnchor.referenceImage)
+        planeNode.opacity = 0.0
+        planeNode.eulerAngles.x = -.pi / 2
+        planeNode.runAction(self.fadeAction)
+        node.addChildNode(planeNode)
         
-//        let planeNode = self.planeNode(withReferenceImage: imageAnchor.referenceImage)
-//        planeNode.opacity = 0.0
-//        planeNode.eulerAngles.x = -.pi / 2
-//        planeNode.runAction(self.fadeAction)
-//        node.addChildNode(planeNode)
-        
-        overlayNode.opacity = 1
+        overlayNode.opacity = 0
         overlayNode.position.y = 0.2
         overlayNode.runAction(self.fadeAndSpinAction)
         
@@ -96,6 +124,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
              self.checkLabel.text = "Image detected: \"\(imageName)\""
         }
     }
+     */
     
     private func planeNode(withReferenceImage image: ARReferenceImage) -> SCNNode {
         let plane = SCNPlane(width: image.physicalSize.width,
@@ -119,7 +148,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         guard let referenceImages = ARReferenceImage.referenceImages(inGroupNamed: "AR Resources", bundle: nil) else {
             fatalError("Missing expected asset catalog resources.")
         }
-        
+    
         configuration.maximumNumberOfTrackedImages = 1
         configuration.trackingImages = referenceImages
         let options: ARSession.RunOptions = [.resetTracking, .removeExistingAnchors]
